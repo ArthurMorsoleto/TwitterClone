ac947dbe518f2aabb92a26c89f6ca6807ed7c531
Arthur M Batista <arthur.morsoleto@gmail.com>
2022-11-01 22:12:10 -0300

    add hilt implementation; add first view model

--- app/src/main/java/com/amb/twitterclone/MainActivity.kt
+++ app/src/main/java/com/amb/twitterclone/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.amb.twitterclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: LoginViewModel by viewModels()
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser?.uid
+        print(user)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAuth.signInWithEmailAndPassword("test@test.com", "123456")
+            .addOnCompleteListener {
+                print(it)
+            }
+            .addOnFailureListener {
+                print(it)
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
     }
 }
