2102e8ad2fa9289e04ff53bd1546870bc4b2afe2
Arthur M Batista <arthur.morsoleto@gmail.com>
2023-02-24 18:55:15 -0300

    add base tweet activity

---
+++ app/src/main/java/com/amb/twitterclone/ui/tweet/TweetActivity.kt
@@ -0,0 +1,13 @@
+package com.amb.twitterclone.ui.tweet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.amb.twitterclone.R
+
+class TweetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tweet)
+    }
+}
